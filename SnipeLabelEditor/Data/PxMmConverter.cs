c6b3848ed198ab612b6ab386faff84b2d563e894
timo <timob1705@outlook.de>
2023-07-20 07:49:05 +0200

    20.07.2023

---
+++ SnipeLabelEditor/Data/PxMmConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace SnipeLabelEditor.Data
+{
+    public static class PxMmConverter
+    {
+        public static double MmToPx(double mm)
+        {
+            return mm * 3.7795275591;
+        }
+
+        public static double PxToMm(double px)
+        {
+            return px / 3.7795275591;
+        }
+    }
+}
---
+++ SnipeLabelEditor/Migrations/20230627072536_AddMmAndPx.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SnipeLabelEditor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMmAndPx : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Width",
+                table: "Labels",
+                newName: "WidthPx");
+
+            migrationBuilder.RenameColumn(
+                name: "Height",
+                table: "Labels",
+                newName: "WidthMm");
+
+            migrationBuilder.AddColumn<int>(
+                name: "HeightMm",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HeightPx",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HeightMm",
+                table: "Labels");
+
+            migrationBuilder.DropColumn(
+                name: "HeightPx",
+                table: "Labels");
+
+            migrationBuilder.RenameColumn(
+                name: "WidthPx",
+                table: "Labels",
+                newName: "Width");
+
+            migrationBuilder.RenameColumn(
+                name: "WidthMm",
+                table: "Labels",
+                newName: "Height");
+        }
+    }
+}
