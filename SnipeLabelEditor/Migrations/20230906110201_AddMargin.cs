bfeeed097b488433ab52ee9faa5783b8204bd467
timo <timob1705@outlook.de>
2023-09-13 08:28:01 +0200

    Release 1 fix

---
+++ SnipeLabelEditor/Migrations/20230906110201_AddMargin.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SnipeLabelEditor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMargin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MarginBottom",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MarginLeft",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MarginRight",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MarginTop",
+                table: "Labels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MarginBottom",
+                table: "Labels");
+
+            migrationBuilder.DropColumn(
+                name: "MarginLeft",
+                table: "Labels");
+
+            migrationBuilder.DropColumn(
+                name: "MarginRight",
+                table: "Labels");
+
+            migrationBuilder.DropColumn(
+                name: "MarginTop",
+                table: "Labels");
+        }
+    }
+}
